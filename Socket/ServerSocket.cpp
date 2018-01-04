9800735ab7ce3d46fd761df6474b61fed555e32f
RedCyberSamurai <redcybersamurai@noreply.github.com>
2018-01-04 13:04:28 +0100

    - more readability - directory fixes - project build fix

--- Socket/ServerSocket.cpp
+++ Socket/ServerSocket.cpp
@@ -5,16 +5,16 @@ ServerSocket::ServerSocket(WindowsSocket &ws) : file("./settings/server.cfg") {
 }
 
 int ServerSocket::start() {
-	ZeroMemory(&address, sizeof(address));
+	ZeroMemory(&this->address, sizeof(this->address));
 	std::string port = "12345";
 
-	address.ai_family = AF_INET;
-	address.ai_socktype = SOCK_STREAM;
-	address.ai_protocol = IPPROTO_UDP;
+	this->address.ai_family = AF_INET;
+	this->address.ai_socktype = SOCK_STREAM;
+	this->address.ai_protocol = IPPROTO_UDP;
 
 	// resolve address
-	int resolveStatus = getaddrinfo(NULL, "12345", &address, &result);
-	if (result != 0) {
+	int resolveStatus = getaddrinfo(NULL, "12345", &this->address, &this->result);
+	if (this->result != 0) {
 		this->lastError = resolveStatus;
 		printf("Could not resolve server address and port: %d", resolveStatus);
 		ws->terminate();
@@ -22,40 +22,40 @@ int ServerSocket::start() {
 	}
 
 	// create socket listener
-	listener = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
-	if (listener == INVALID_SOCKET) {
+	this->listener = socket(this->result->ai_family, this->result->ai_socktype, this->result->ai_protocol);
+	if (this->listener == INVALID_SOCKET) {
 		this->lastError = WSAGetLastError();
 		printf("Could not start the listener: %d", WSAGetLastError());
 		freeaddrinfo(result);
-		ws->terminate();
+		this->ws->terminate();
 		return LISTEN_ERROR;
 	}
 
 	// bind server
-	int bindStatus = bind(listener, result->ai_addr, (int)result->ai_addrlen);
+	int bindStatus = bind(this->listener, this->result->ai_addr, (int)this->result->ai_addrlen);
 	if (bindStatus == SOCKET_ERROR) {
 		this->lastError = WSAGetLastError();
 		printf("Could not bind the socket to the given address and port: %d", WSAGetLastError());
-		freeaddrinfo(result);
-		closesocket(listener);
-		ws->terminate();
+		freeaddrinfo(this->result);
+		closesocket(this->listener);
+		this->ws->terminate();
 		return BIND_ERROR;
 	}
 
 	// listen socket
-	if (listen(listener, SOMAXCONN) == SOCKET_ERROR) {
+	if (listen(this->listener, SOMAXCONN) == SOCKET_ERROR) {
 		this->lastError = WSAGetLastError();
 		printf("Could not listen to socket connection: %d", WSAGetLastError());
-		closesocket(listener);
-		ws->terminate();
+		closesocket(this->listener);
+		this->ws->terminate();
 		return LISTEN_SOCKET_ERROR;
 	}
 
 	// accept client (TODO thread)
-	client = socket(listener, NULL, NULL);
-	if (client == INVALID_SOCKET) {
-		closesocket(listener);
-		ws->terminate();
+	this->client = socket(this->listener, NULL, NULL);
+	if (this->client == INVALID_SOCKET) {
+		closesocket(this->listener);
+		this->ws->terminate();
 		return ACCEPT_ERROR;
 	}
 
